2c74a433db58fdb10e19dbcffc9abc748387de74
Charalambos Hadjiantoniou <charhadj@gmail.com>
2022-01-06 20:20:39 +0200

    Refactoring, added test for auditing

--- Source/Infrastructure/CH.CleanArchitecture.Infrastructure/Models/DataEntityBase.cs
+++ Source/Infrastructure/CH.CleanArchitecture.Infrastructure/Models/DataEntityBase.cs
@@ -4,6 +4,7 @@ namespace CH.CleanArchitecture.Infrastructure.Models
 {
     /// <summary>
     /// Base data entity class to be inherited from every entity in database context
+    /// Inherits <see cref="AuditableEntity"/> for convenience.
     /// </summary>
     public class DataEntityBase<TId> : AuditableEntity, IDataEntity<TId>
     {
--- Tests/CH.CleanArchitecture.Infrastructure.Tests/AuditingTests.cs
+++ Tests/CH.CleanArchitecture.Infrastructure.Tests/AuditingTests.cs
@@ -1,12 +1,22 @@
-﻿using Xunit;
+﻿using System.Linq;
+using CH.CleanArchitecture.Infrastructure.Models;
+using CH.CleanArchitecture.Tests;
+using Xunit;
 
 namespace CH.CleanArchitecture.Infrastructure.Tests
 {
-    public class AuditingTests
+    public class AuditingTests : TestBase
     {
         [Fact]
-        public void Auditing_OnAdded_CreatesAuditHistoryRecord() {
+        public void Auditing_OnAdd_AuditableEntity_CreatesAuditHistoryRecord()
+        {
+            OrderEntity order = new OrderEntity();
+            ApplicationContext.Orders.Add(order);
+            ApplicationContext.SaveChanges();
 
+            var auditHistory = ApplicationContext.AuditHistory.SingleOrDefault();
+
+            Assert.NotNull(auditHistory);
         }
     }
 }
