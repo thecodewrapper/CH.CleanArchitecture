075a63413b9eee4e7abd1a4ec9b2f4eb562b8cfb
Charalambos Hadjiantoniou <charhadj@gmail.com>
2022-01-14 22:57:49 +0200

    .

--- Source/Core/CH.CleanArchitecture.Core.Application/Interfaces/Repositories/Domain/IOrderRepository.cs
+++ Source/Core/CH.CleanArchitecture.Core.Application/Interfaces/Repositories/Domain/IOrderRepository.cs
@@ -3,7 +3,7 @@ using CH.CleanArchitecture.Core.Domain.Entities.OrderAggregate;
 
 namespace CH.CleanArchitecture.Core.Application
 {
-    public interface IOrderRepository : IEntityRepository<Order, Guid>
+    public interface IOrderRepository : IAggregateRepository<Order, Guid>
     {
     }
 }
