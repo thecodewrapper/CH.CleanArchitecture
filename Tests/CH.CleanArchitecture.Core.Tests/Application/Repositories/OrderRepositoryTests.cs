cc04aa2da7bfdaf19b889790b44fdaf4a7652d24
Charalampos Hadjiantoniou <charhadj@gmail.com>
2022-01-16 13:08:50 +0200

    Added tests for order repository event store persistence

--- Tests/CH.CleanArchitecture.Core.Tests/Application/Repositories/OrderRepositoryTests.cs
+++ Tests/CH.CleanArchitecture.Core.Tests/Application/Repositories/OrderRepositoryTests.cs
@@ -46,5 +46,36 @@ namespace CH.CleanArchitecture.Core.Tests.Application.Repositories
 
             Assert.Equal(trackingNumber, ((OrderCreatedEvent)events.Single()).TrackingNumber);
         }
+
+        [Fact]
+        public async Task SaveToEventStore_AddOrderItem_OrderItemAddedEvent_ExistsInStore()
+        {
+            var order = new Order("1234");
+            order.AddOrderItem("Product name", 100, 2);
+            await _orderRepository.SaveToEventStoreAsync(order);
+
+            var events = await _eventStore.LoadAsync<Guid>(order.Id.ToString(), nameof(Order), 0, int.MaxValue);
+
+            Assert.Contains(events, (e) => e is OrderItemAddedEvent);
+        }
+
+        [Fact]
+        public async Task SaveToEventStore_AddOrderItem_OrderItemAddedEvent_OrderItemDetails_SameAsOrderItem()
+        {
+            string productName = "Product name";
+            decimal price = 100;
+            int quantity = 2;
+            var order = new Order("1234");
+            order.AddOrderItem(productName, price, quantity);
+            await _orderRepository.SaveToEventStoreAsync(order);
+
+            var events = await _eventStore.LoadAsync<Guid>(order.Id.ToString(), nameof(Order), 0, int.MaxValue);
+
+            var orderItemAddedEvent = (OrderItemAddedEvent)events.Single(e => e is OrderItemAddedEvent);
+
+            Assert.Equal(productName, orderItemAddedEvent.ProductName);
+            Assert.Equal(price, orderItemAddedEvent.ProductPrice);
+            Assert.Equal(quantity, orderItemAddedEvent.Quantity);
+        }
     }
 }
